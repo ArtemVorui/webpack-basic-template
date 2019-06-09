5c87e6fdfc1ae0c07e582859ea738f9fae5d6a31
Artem_Vorui <artemvoruy@gmail.com>
2019-06-09 17:19:59 +0300

    rewrote webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,91 @@
-const webpack = require('webpack');
-const path = require('path');
-const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+'use strict';
 
-const config = {
-  entry: './src/index.js',
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[contenthash].js',
+    filename: 'bundle.[contenthash:8].js',
+  },
+  resolve: {
+    alias: {
+      '@fonts': path.resolve(__dirname, './src/assets/fonts'),
+      '@images': path.resolve(__dirname, './src/assets/images'),
+      '@scss': path.resolve(__dirname, './src/stylesheets/scss'),
+    },
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
       },
       {
-        test: /\.css$/,
+        test: /.s?css$/,
         use: [
-          'style-loader',
-          'css-loader',
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('node-sass'),
+              outputPath: '@scss',
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: { implementation: require('autoprefixer') },
+          },
         ],
       },
       {
-        test: /\.scss$/,
+        test: /\.(png|jpg|gif|svg|ico)$/,
         use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: '@images',
+            },
+          },
         ],
-      },
-      {
-        test: /\.svg$/,
-        use: 'file-loader',
       },
       {
         test: /\.png$/,
         use: [
           {
             loader: 'url-loader',
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-url-loader',
+        options: {
+          noquotes: true,
+        },
+      },
+      {
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
             options: {
-              mimetype: 'image/png',
+              name: '[name].[ext]',
+              outputPath: '@fonts',
             },
           },
         ],
@@ -49,26 +93,25 @@ const config = {
     ],
   },
   plugins: [
-    new webpack.ContextReplacementPlugin(/moment[\/\\]locale$/, /en/),
-    new LodashModuleReplacementPlugin,
     new HtmlWebpackPlugin({
-      template: require('html-webpack-template'),
+      title: 'My App',
       inject: false,
-      appMountId: 'app',
+      template: require('html-webpack-template'),
+      links: [
+        {
+          href: '@images/favicon.ico',
+          rel: 'shortcut icon',
+          sizes: '16x16',
+          type: 'image/x-icon',
+        },
+      ],
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.[contenthash:8].css',
     }),
   ],
-  optimization: {
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\\/]node_modules[\\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
-  },
 };
-
-module.exports = config;
